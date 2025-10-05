9143ee525864189014f4f87d08c1949ac74595c4
Shamit Soni <shamit.soni4@gmail.com>
2025-10-04 20:06:48 -0400

    New feature: hypertension
    - Modify pre-processing script to extract blood pressure values
      (systolic, diastolic)
    - Create hypertension feature to store binary indication of whether an
      individual is hypertensive
      	- true = 1, false = 0
    	- hypertensive: systolic >= 130 or diastolic >= 80

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
+# Fill in blank cells and use consistent naming conventions for features
 df = pd.read_csv("../data/sleep_health.csv")
 df['Sleep Disorder'] = df['Sleep Disorder'].fillna('None')
 df.columns = [col.strip().lower().replace(" ", "_") for col in df.columns]
+
+# Extract data for new features
+
+# 1. Hypertension
+df[["systolic", "diastolic"]] = df["blood_pressure"].str.split("/", expand=True).astype(int)
+df["hypertension"] = ((df["systolic"] >= 130) | (df["diastolic"] >= 80)).astype(int)
+
+
+# Export processed data
 df.to_csv("../data/sleep_health_clean.csv", index=False)
